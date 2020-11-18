1c18d98c001af069a1c051a0a3a52485aade00e2
kyle-c213 <k.callander@sastel.net>
2020-11-17 21:59:51 -0600

    changes to get professor search pageto mostly display properly

--- project/resources/views/profRate/search.blade.php
+++ project/resources/views/profRate/search.blade.php
@@ -1,29 +1,33 @@
 @extends('Layouts.app')
 
-{{-- @section('title')
-    {{$title}}
-@endsection --}}
+<script>
+    function profResult(){
+        var display = document.getElementById(profDisplay);
+    }
+
+    /*blank to display profs OR show all then narrow(onchnage clear and re-display)*/
+</script>
 
 <!--Left Sidebar-->
 @include('inc.sidebar')
 
 <!--Main body content-->
 @section('content')
-    <div class="container" id="body">
+    <div class="container " id="body">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div name="PSearch" class="container">
                     <form name="profSearch" id="profSearch" action="search.blade.php" method="POST">
                         <h5>Search a Professor to Rate</h5><br>
                         <input type="hidden" name="submitted" id="submitted" value="1">
-                        <input type="text" name="Psearchbar" id="Psearchbar" placeholder="Search for a professor here" style=" width: 200px;">
+                        <input type="text" name="Psearchbar" id="Psearchbar" placeholder="Search for a professor here" style=" width: 200px;" onkeyup="profResult">
                         <input type="submit" value="Search" style="width: 70px;">
                     </form>
                 </div>
                 <div name="PResult" class="container">
                     <h5>Current Results</h5>
                     <!--list of all profs to start, once searched list is reduced, says not profs found-->
-        
+                    <p id="profDisplay"> </p>
                 </div>
             </div>
         </div>
