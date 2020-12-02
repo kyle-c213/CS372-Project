486cf540c5e22988b7828f23eea5bb844b2861a0
Nicholas Strelic <nstrelic@gmail.com>
2020-12-02 14:57:38 -0600

    Comments and small changes to mail send page

--- project/resources/views/Mails/send.blade.php
+++ project/resources/views/Mails/send.blade.php
@@ -12,17 +12,16 @@
 </style>
 
 <script>
+    // template message
     window.onload = function() {
         $("#title").val("Concerning " + "\"{{$listing->title}}\"");
         $("#body").val("Hi " + "{{\App\Models\User::where('id', $user->id)->first()->name}},\n" + "I am interest in purchasing this item.\n" + "Thanks,\n" + "{{\App\Models\User::where('id', auth()->user()->id)->first()->name}}");
     };
-    // $(document).ready(function(){
-        
-    // });
 </script>
 
 @section('content')
 
+<!-- Mail Form -->
 <form action="{{route('mail.send_post')}}" method="POST">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <input type="hidden" name="to_id" value="{{$user->id}}"/>
@@ -39,7 +38,7 @@
         <textarea name="body" id="body" class="form-control"></textarea>
     </div>
     <div class="form-group">
-        <input type="submit" class="btn btn-primary" value="Send Email" onclick="warn()"/>
+        <input type="submit" class="btn btn-primary" value="Send Message" onclick="warn()"/>
     </div>
 </form>
 
